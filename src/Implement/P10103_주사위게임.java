d54afd76c133e7bb1a51133713e1f884b79a2145
codingHack <dbstjdwo1000@naver.com>
2019-09-30 13:58:33 +0900

    “문제풀이”

--- src/Implement/P10103_주사위게임.java
+++ src/Implement/P10103_주사위게임.java
@@ -8,27 +8,27 @@ public class P10103_주사위게임 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N=Integer.parseInt(br.readLine());
-		
-		int a=100;
-		int b=100;
-		
-		for(int i=0;i<N;i++) {
-			String s[]=br.readLine().split(" ");
-			int an=Integer.parseInt(s[0]);
-			int bn=Integer.parseInt(s[1]);
-			
-			if(an>bn) {
-				b-=an;
-			}else if(an<bn) {
-				a-=bn;
+
+		int N = Integer.parseInt(br.readLine());
+
+		int a = 100;
+		int b = 100;
+
+		for (int i = 0; i < N; i++) {
+			String s[] = br.readLine().split(" ");
+			int an = Integer.parseInt(s[0]);
+			int bn = Integer.parseInt(s[1]);
+
+			if (an > bn) {
+				b -= an;
+			} else if (an < bn) {
+				a -= bn;
 			}
 		}
-		
+
 		System.out.println(a);
 		System.out.println(b);
-		
+
 	}
 
 }
