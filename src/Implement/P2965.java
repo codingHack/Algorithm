8baaf05246357f46ddf39617e4317a691c0a25e9
codingHack <dbstjdwo1000@naver.com>
2019-10-13 15:51:38 +0900

    “문제풀이”

---
+++ src/Implement/P2965.java
@@ -0,0 +1,24 @@
+package Implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P2965 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+
+		int A=Integer.parseInt(st.nextToken());
+		int B=Integer.parseInt(st.nextToken());
+		int C=Integer.parseInt(st.nextToken());
+		
+		
+		System.out.println(Math.max(C-B, B-A)-1);
+	}
+
+}
