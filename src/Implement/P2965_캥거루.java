511d4ca33852d0f9b54b06d7d4e57798d595809c
codingHack <dbstjdwo1000@naver.com>
2019-10-13 15:57:25 +0900

    “문제풀이”

--- src/Implement/P2965_캥거루.java
+++ src/Implement/P2965_캥거루.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class P2965 {
+public class P2965_캥거루 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -13,12 +13,11 @@ public class P2965 {
 		StringTokenizer st;
 		st = new StringTokenizer(br.readLine());
 
-		int A=Integer.parseInt(st.nextToken());
-		int B=Integer.parseInt(st.nextToken());
-		int C=Integer.parseInt(st.nextToken());
-		
-		
-		System.out.println(Math.max(C-B, B-A)-1);
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+
+		System.out.println(Math.max(C - B, B - A) - 1);
 	}
 
 }
---
+++ src/Implement/P9325_얼마.java
@@ -0,0 +1,35 @@
+package Implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P9325_얼마 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		
+		int T=Integer.parseInt(st.nextToken());
+		
+		for(int i=0;i<T;i++) {
+			int sum=0;
+			st = new StringTokenizer(br.readLine());
+			sum+=Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine());
+			int N=Integer.parseInt(st.nextToken());
+			for(int j=0;j<N;j++) {
+				st = new StringTokenizer(br.readLine());
+				int a=Integer.parseInt(st.nextToken());
+				int b=Integer.parseInt(st.nextToken());
+				sum+=(a*b);
+			}
+			System.out.println(sum);
+			
+		}
+	}
+
+}
