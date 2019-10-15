4db5cd95ad5e3b3eb7ce6682bce181a6d2b9f16e
codingHack <dbstjdwo1000@naver.com>
2019-10-16 01:56:20 +0900

    “문제풀이”

--- src/Array/P10988_팰린드롬.java
+++ src/Array/P10988_팰린드롬.java
@@ -1,22 +1,37 @@
-package Basic;
+package Array;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.math.BigInteger;
 import java.util.StringTokenizer;
 
-public class P10757_큰수 {
+public class P10988_팰린드롬 {
 	static public void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 		st = new StringTokenizer(br.readLine());
-		BigInteger ba = new BigInteger(st.nextToken());
-		BigInteger bb = new BigInteger(st.nextToken());
+		String s = st.nextToken();
 
-		System.out.println(ba.add(bb).toString());
+		int start = 0;
+		int end = s.length() - 1;
+
+		boolean can = true;
+		while (true) {
+			if (start >= end) {
+				break;
+			} else {
+				if (!(s.substring(start, start + 1).equals(s.substring(end, end + 1)))) {
+					can = false;
+					break;
+				}
+			}
+			start++;
+			end--;
+		}
+		
+		System.out.println(can?1:0);
 	}
 }
