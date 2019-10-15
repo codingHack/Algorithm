aa99ba9c398160dbd3756344b3503f85730d81fa
codingHack <dbstjdwo1000@naver.com>
2019-10-16 01:49:14 +0900

    “문제풀이”

---
+++ src/Brute/P1969_DNA.java
@@ -0,0 +1,58 @@
+package Brute;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class P1969_DNA {
+	static public int N;
+	static public int M;
+	static public String []dna= {"A","C","G","T"};
+	static public String []arr;
+
+	static public void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		arr= new String[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = br.readLine();
+		}
+		
+		String s="";
+		int result=0;
+		
+		for(int i=0;i<M;i++) {
+			int min=Integer.MAX_VALUE;
+			int minIndex=0;
+			for(int j=0;j<4;j++) {
+				int sum=0;
+				for(int k=0;k<N;k++) {
+					if(!arr[k].substring(i,i+1).equals(dna[j])) {
+						sum++;
+					}
+				}
+				if(sum<min) {
+					min=sum;
+					minIndex=j;
+				}else if(sum==min) {
+					minIndex=Math.min(minIndex, j);
+				}
+			}
+			s+=dna[minIndex];
+			result+=min;
+		}
+		System.out.println(s);
+		System.out.println(result);
+		
+
+	}
+}
--- src/DFSBFS/P15663_N과M9.java
+++ src/DFSBFS/P15663_N과M9.java
@@ -17,41 +17,42 @@ public class P15663_N과M9 {
 	static public int arr[];
 	static public boolean visited[];
 	static public LinkedHashMap<String, Integer> m;
-	
-	static public void dfs(int depth, int v,String s) {
-		if(depth==M) {
+
+	static public void dfs(int depth, int v, String s) {
+		if (depth == M) {
 			m.put(s, 0);
-		}else {
-			visited[v]=true;
-			for(int i=0;i<N;i++) {
-				if(!visited[i]) {					
-					dfs(depth+1,i,s+" "+arr[i]);
+		} else {
+			visited[v] = true;
+			for (int i = 0; i < N; i++) {
+				if (!visited[i]) {
+					dfs(depth + 1, i, s + " " + arr[i]);
 				}
 			}
 		}
-		visited[v]=false;
+		visited[v] = false;
 	}
-	static public void main(String []args) throws IOException {
+
+	static public void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 		st = new StringTokenizer(br.readLine());
-		N=Integer.parseInt(st.nextToken());
-		M=Integer.parseInt(st.nextToken());
-		
-		m=new LinkedHashMap<String, Integer>();
-		arr=new int[N];
-		visited=new boolean[N];
-		String s[]=br.readLine().split(" ");
-		for(int i=0;i<s.length;i++) {
-			arr[i]=Integer.parseInt(s[i]);
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		m = new LinkedHashMap<String, Integer>();
+		arr = new int[N];
+		visited = new boolean[N];
+		String s[] = br.readLine().split(" ");
+		for (int i = 0; i < s.length; i++) {
+			arr[i] = Integer.parseInt(s[i]);
 		}
 		Arrays.sort(arr);
-		for(int i=0;i<N;i++) {
-			dfs(1,i,""+arr[i]);
+		for (int i = 0; i < N; i++) {
+			dfs(1, i, "" + arr[i]);
 		}
-		
-		for(String a:m.keySet()) {
+
+		for (String a : m.keySet()) {
 			System.out.println(a);
 		}
 	}
