fdf5a455df68b97031ed0ab86c0f8f8a25df17fb
codingHack <dbstjdwo1000@naver.com>
2019-10-17 00:05:00 +0900

    “문제풀이”

---
+++ src/Array/P2857_FBI.java
@@ -0,0 +1,36 @@
+package Array;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class P2857_FBI {
+	static public void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int sum = 0;
+		for (int i = 0; i < 5; i++) {
+			String s = br.readLine();
+			if (s.length() > 10) {
+				continue;
+			}
+			for (int j = 0; j < s.length() - 2; j++) {
+				if (s.charAt(j) == 'F' && s.charAt(j + 1) == 'B' && s.charAt(j + 2) == 'I') {
+					sum++;
+					bw.write(i + 1 + " ");
+					break;
+				}
+			}
+
+		}
+		if (sum == 0) {
+			bw.write("HE GOT AWAY!\n");
+		}
+		
+		bw.flush();
+	}
+}
--- src/Basic/P11023_더하기.java
+++ src/Basic/P11023_더하기.java
@@ -7,14 +7,14 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class P11023_더하기 {
-	static public void main(String []args) throws IOException {
+	static public void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
-		int  sum=0;
-		
-		String s[]=br.readLine().split(" ");
-		for(int i=0;i<s.length;i++) {
-			sum+=Integer.parseInt(s[i]);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int sum = 0;
+
+		String s[] = br.readLine().split(" ");
+		for (int i = 0; i < s.length; i++) {
+			sum += Integer.parseInt(s[i]);
 		}
 		System.out.println(sum);
 	}
