5440d69be2e7653a312f114bf3c5c41f677e088c
codingHack <dbstjdwo1000@naver.com>
2019-10-16 23:41:51 +0900

    “문제풀이”

--- src/Basic/P11023_더하기.java
+++ src/Basic/P11023_더하기.java
@@ -1,5 +1,21 @@
 package Basic;
 
-public class P11023_더하기 {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
+public class P11023_더하기 {
+	static public void main(String []args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+		int  sum=0;
+		
+		String s[]=br.readLine().split(" ");
+		for(int i=0;i<s.length;i++) {
+			sum+=Integer.parseInt(s[i]);
+		}
+		System.out.println(sum);
+	}
 }
