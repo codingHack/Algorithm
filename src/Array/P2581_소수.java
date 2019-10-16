b94b32300bed55ce5754d72c394c9eb16d364103
codingHack <dbstjdwo1000@naver.com>
2019-10-17 00:39:01 +0900

    “문제풀이”

---
+++ src/Array/P2581_소수.java
@@ -0,0 +1,46 @@
+package Array;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class P2581_소수 {
+	static public void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int M = Integer.parseInt(st.nextToken());
+
+		boolean eratos[] = new boolean[M + 1];
+
+		for (int i = 2; i <= M / 2; i++) {
+			if(!eratos[i]) {
+				for(int j=2;j*i<=M;j++) {
+					eratos[j*i]=true;
+				}
+			}
+		}
+		
+		int sum=0;
+		int min=M+1;
+		
+		for(int i=N;i<=M;i++) {
+			if(!eratos[i]) {
+				sum+=i;
+				min=Math.min(min, i);
+			}
+		}
+		if(sum==0) {
+			System.out.println(-1);
+		}else {
+			System.out.println(sum);
+			System.out.println(min);
+		}
+	}
+}
--- src/Array/P7600_문자몇갤까.java
+++ src/Array/P7600_문자몇갤까.java
@@ -1,5 +1,32 @@
 package Array;
 
-public class P7600_문자몇갤까 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class P7600_문자몇갤까 {
+	static public void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			boolean arr[] = new boolean[27];
+			String s = br.readLine().toUpperCase();
+			if (s.equals("#")) {
+				break;
+			}
+
+			for (int i = 0; i < s.length(); i++) {
+				char c = s.charAt(i);
+				if (c >= 'A' && c <= 'Z') {
+					arr[c - 65] = true;
+				}
+			}
+
+			int sum = 0;
+			for (int i = 0; i < 27; i++) {
+				if (arr[i])
+					sum++;
+			}
+			System.out.println(sum);
+		}
+	}
 }
