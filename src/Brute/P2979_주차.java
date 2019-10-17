6679d72e0062d7f1c83d142801c4397c03575457
codingHack <dbstjdwo1000@naver.com>
2019-10-17 15:43:47 +0900

    “문제풀이”

---
+++ src/Brute/P2979_주차.java
@@ -0,0 +1,43 @@
+package Brute;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class P2979_주차 {
+	static public void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+
+		int arr[] = new int[101];
+		System.out.println();
+		for (int i = 0; i < 3; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			for (int j = a; j < b; j++) {
+				arr[j]++;
+			}
+		}
+		int sum = 0;
+		for (int i = 1; i < 101; i++) {
+			if (arr[i] == 1) {
+				sum += A;
+			} else if (arr[i] == 2) {
+				sum += 2 * B;
+			} else if (arr[i] == 3) {
+				sum += 3 * C;
+			}
+		}
+		System.out.println(sum);
+
+	}
+}
--- src/Brute/P2998_8진수.java
+++ src/Brute/P2998_8진수.java
@@ -5,40 +5,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class P2998_8진수 {
-	static public void main(String []args) throws IOException {
+	static public void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String s=br.readLine();
-		
-		if(s.length()%3==1) {
-			s="00"+s;
+
+		String s = br.readLine();
+
+		if (s.length() % 3 == 1) {
+			s = "00" + s;
 		}
-		if(s.length()%3==2) {
-			s="0"+s;
+		if (s.length() % 3 == 2) {
+			s = "0" + s;
 		}
-		
-		String result="";
-		for(int i=0;i<s.length()-2;i+=3) {
-			String ss=s.substring(i, i+3);
-			if(ss.equals("000")) {
-				result+="0";
-			}else if(ss.equals("001")) {
-				result+="1";
-			}else if(ss.equals("010")) {
-				result+="2";
-			}else if(ss.equals("011")) {
-				result+="3";
-			}else if(ss.equals("100")) {
-				result+="4";
-			}else if(ss.equals("101")) {
-				result+="5";
-			}else if(ss.equals("110")) {
-				result+="6";
-			}else if(ss.equals("111")) {
-				result+="7";
+
+		String result = "";
+		for (int i = 0; i < s.length() - 2; i += 3) {
+			String ss = s.substring(i, i + 3);
+			if (ss.equals("000")) {
+				result += "0";
+			} else if (ss.equals("001")) {
+				result += "1";
+			} else if (ss.equals("010")) {
+				result += "2";
+			} else if (ss.equals("011")) {
+				result += "3";
+			} else if (ss.equals("100")) {
+				result += "4";
+			} else if (ss.equals("101")) {
+				result += "5";
+			} else if (ss.equals("110")) {
+				result += "6";
+			} else if (ss.equals("111")) {
+				result += "7";
 			}
 		}
-		
+
 		System.out.println(result);
 	}
 }
